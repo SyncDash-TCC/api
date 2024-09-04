b98cab3c5bd6c8000cb6de901d356c36623d42b1
FelipeMassashi <felipemassashi@gmail.com>
2024-09-03 23:41:27 -0300

    feat: add post login

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,15 @@ from app.depends import get_db_session, token_verifier
 from app.auth_user import UserUseCases
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
 
-user_router = APIRouter(prefix='/user')
+user_router = APIRouter(prefix='/auth')
 test_router = APIRouter(prefix='/test', dependencies=[Depends(token_verifier)])
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
 
 
 @user_router.post('/register')
@@ -27,13 +32,13 @@ def user_register(
 
 @user_router.post('/login')
 def user_login(
-    request_form_user: OAuth2PasswordRequestForm = Depends(),
+    login_request: LoginRequest,
     db_session: Session = Depends(get_db_session),
 ):
     uc = UserUseCases(db_session=db_session)
     user = User(
-        username=request_form_user.username,
-        password=request_form_user.password
+        username=login_request.username,
+        password=login_request.password
     )
 
     auth_data = uc.user_login(user=user)
